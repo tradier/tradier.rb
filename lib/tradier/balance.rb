f279b995a2200c72b9fd6057f6d912bdd5a0a4cb
Steve Agalloco <steve.agalloco@gmail.com>
2013-06-21 01:15:02 -0400

    account balance payload update

--- lib/tradier/balance.rb
+++ lib/tradier/balance.rb
@@ -2,5 +2,12 @@ require 'tradier/base'
 
 module Tradier
   class Balance < Tradier::Base
+
+    attr_reader :account_number, :account_type, :cash_available, \
+    :cash_margin_value, :dividend_balance, :equity_value, :long_liquid_value, \
+    :long_market_value, :market_value, :net_value, :option_requirement, \
+    :prev_cash_available, :prev_maintenance_call, :short_liquid_value, \
+    :short_market_value, :sweep, :accumFedAmt, :accumFedPrev, :bopmvEqty, \
+    :cashMgnCashAv, :optnAddtlReqAmt, :currType
   end
 end
