02e988abcb1b7a7e67015e41898acc656829c17d
Steve Agalloco <steve.agalloco@gmail.com>
2013-06-21 01:14:11 -0400

    added volume, bid and ask size to option quote

--- lib/tradier/option_quote.rb
+++ lib/tradier/option_quote.rb
@@ -3,7 +3,7 @@ require 'tradier/base'
 module Tradier
   class OptionQuote < Base
 
-    attr_reader :symbol, :strike, :last, :bid, :ask, :change, :open_interest
+    attr_reader :symbol, :strike, :last, :bid, :ask, :change, :open_interest, :bid_size, :ask_size, :volume
 
     def ==(option_quote)
       self.symbol == option_quote.symbol
