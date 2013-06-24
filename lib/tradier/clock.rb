e2a7418f6a4992870d40a915dfea60a579e1889a
Steve Agalloco <steve.agalloco@gmail.com>
2013-06-24 16:07:12 -0400

    implement attribute readers for Tradier::Clock

--- lib/tradier/clock.rb
+++ lib/tradier/clock.rb
@@ -2,5 +2,20 @@ require 'tradier/base'
 
 module Tradier
   class Clock < Tradier::Base
+
+    attr_reader :description, :next_change, :next_state, :state
+
+    def date
+      return unless @date || @attrs[:date]
+
+      @date ||= Date.parse(@attrs[:date])
+    end
+
+    def time
+      return unless @time || @attrs[:timestamp]
+
+      @time ||= Time.at(@attrs[:timestamp].to_i)
+    end
+
   end
 end
---
+++ spec/clock_spec.rb
@@ -0,0 +1,73 @@
+require 'spec_helper'
+
+describe Tradier::Clock do
+
+  describe "#date" do
+    it "returns the date when set" do
+      clock = Tradier::Clock.new({ :date => '2013-07-04' })
+      expect(clock.date).to be_a Date
+      expect(clock.date.to_s).to eq('2013-07-04')
+    end
+    it "returns nil when symbol is not set" do
+      clock = Tradier::Clock.new('clock' => {})
+      expect(clock.date).to be_nil
+    end
+  end
+
+  describe "#time" do
+    it "returns the timestamp when set" do
+      clock = Tradier::Clock.new({ :timestamp => 1372103978 })
+      expect(clock.time).to be_a Time
+      expect(clock.time.to_s).to eq('2013-06-24 15:59:38 -0400')
+    end
+    it "returns nil when symbol is not set" do
+      clock = Tradier::Clock.new('clock' => {})
+      expect(clock.time).to be_nil
+    end
+  end
+
+  describe "#description" do
+    it "returns the date when set" do
+      clock = Tradier::Clock.new({ :description => 'Market is closed.' })
+      expect(clock.description).to eq('Market is closed.')
+    end
+    it "returns nil when symbol is not set" do
+      clock = Tradier::Clock.new('clock' => {})
+      expect(clock.description).to be_nil
+    end
+  end
+
+  describe "#next_change" do
+    it "returns the date when set" do
+      clock = Tradier::Clock.new({ :next_change => '08:00' })
+      expect(clock.next_change).to eq('08:00')
+    end
+    it "returns nil when symbol is not set" do
+      clock = Tradier::Clock.new('clock' => {})
+      expect(clock.next_change).to be_nil
+    end
+  end
+
+  describe "#next_state" do
+    it "returns the date when set" do
+      clock = Tradier::Clock.new({ :next_state => 'premarket' })
+      expect(clock.next_state).to eq('premarket')
+    end
+    it "returns nil when symbol is not set" do
+      clock = Tradier::Clock.new('clock' => {})
+      expect(clock.next_state).to be_nil
+    end
+  end
+
+  describe "#state" do
+    it "returns the date when set" do
+      clock = Tradier::Clock.new({ :state => 'closed' })
+      expect(clock.state).to eq('closed')
+    end
+    it "returns nil when symbol is not set" do
+      clock = Tradier::Clock.new('clock' => {})
+      expect(clock.state).to be_nil
+    end
+  end
+
+end
