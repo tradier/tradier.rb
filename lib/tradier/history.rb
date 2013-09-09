573ef051b07bef7da0f7ceff41da8d350e919f33
Steve Agalloco <steve.agalloco@gmail.com>
2013-09-09 11:51:05 -0400

    added attr_readers to Tradier::History

--- lib/tradier/history.rb
+++ lib/tradier/history.rb
@@ -1,7 +1,16 @@
 require 'tradier/base'
+require 'date'
 
 module Tradier
   class History < Tradier::Base
+
+    attr_reader :open , :close, :high, :low, :volume
+
+    def date
+      return unless @date || @attrs[:date]
+
+      @date ||= Date.parse(@attrs[:date])
+    end
 
     def self.from_response(body={})
       new(body)
