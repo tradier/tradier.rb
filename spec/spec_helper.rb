f81094e1c9d8beddde8dfe5791ace9574a11fc28
Steve Agalloco <steve.agalloco@gmail.com>
2013-11-08 10:43:33 -0500

    adjust spec helper to use coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,16 @@
 # encoding: utf-8
-unless ENV['CI']
-  require 'simplecov'
-  SimpleCov.start do
-    add_filter '.bundle'
-    add_group 'Tradier', 'lib/tradier'
-    add_group 'Specs', 'spec'
-  end
+
+require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start do
+  add_group 'Tradier', 'lib/tradier'
+  add_group 'Specs', 'spec'
+  add_filter '.bundle'
 end
 
 require 'tradier'
