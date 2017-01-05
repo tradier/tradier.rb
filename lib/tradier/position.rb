1c05460ee67d06d2e7896d1f4c5b063409d90565
stve <steve.agalloco@gmail.com>
2017-01-05 14:47:52 -0500

    update cost_basis field on positions

--- lib/tradier/position.rb
+++ lib/tradier/position.rb
@@ -2,6 +2,6 @@ require 'tradier/base'
 
 module Tradier
   class Position < Tradier::Base
-    attr_reader :costbasis, :date_acquired, :quantity, :symbol
+    attr_reader :cost_basis, :date_acquired, :quantity, :symbol
   end
 end
